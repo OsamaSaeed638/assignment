18c40d2edb46a991020e12c6cd8874f14b56955f
Osama Saeed <oosamasaeed893@gmail.com>
2024-09-06 15:48:46 +0500

    class assignment

---
+++ app.js
@@ -0,0 +1,25 @@
+//  Variables
+
+
+// Bio  data using JS 
+// var firstname = "Osama"
+
+
+// var lastname = "Saeed"
+
+
+// var fullname = "Osama Saeed"
+
+
+// var city = "Karachi"
+
+
+// var Education = "Undergraduate"
+
+
+// console.log( 
+//     "Hey my name is Osama Saeed. I live in karachi. I am an undergraduate right now ")
+
+
+//     console.log( 
+//         "Hey my name is " + fullname + ". I live in " + city + ". I am an " + Education + "right now")
